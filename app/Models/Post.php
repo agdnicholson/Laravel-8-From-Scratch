234129e410821306e263551827c7731312363dd9
Andrew Nicholson <agdnicholson@gmail.com>
2022-02-12 16:18:23 +0000

    Section 2: The Basics done.

---
+++ app/Models/Post.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Models;
+
+//use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\File;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
+
+class Post
+{
+    //use HasFactory;
+
+    public $title;
+    public $excerpt;
+    public $date;
+    public $body;
+    public $slug;
+
+    public function __construct($title, $excerpt, $date, $body, $slug)
+    {
+        $this->title = $title;
+        $this->excerpt = $excerpt;
+        $this->date = $date;
+        $this->body = $body;
+        $this->slug = $slug;
+    }
+
+    public static function all()
+    {
+        return cache()->rememberForever('posts.all', function() {
+            return collect(File::files(resource_path("posts")))
+            ->map(function($file) {
+                return YamlFrontMatter::parseFile($file);
+            })
+            ->map(function($document) {
+                return new Post(
+                    $document->title,
+                    $document->excerpt,
+                    $document->date,
+                    $document->body(),
+                    $document->slug,
+                );
+            })
+            ->sortByDesc('date');
+        });
+    }
+
+    public static function find($slug)
+    {
+        return Post::all()->firstWhere('slug', $slug);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,11 +15,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('posts');
-});
 
-Route::get('post', function () {
-    return view('post', [
-        'post' => '<h1>Hello World</h1>'
+    return view('posts', [
+        'posts' => Post::all()
     ]);
 });
+
+Route::get('posts/{post}', function ($slug) {
+    $post = Post::find($slug);
+    return view('post', [
+        'post' => $post
+    ]);
+})->where('post', '[A-z_\-]+');
