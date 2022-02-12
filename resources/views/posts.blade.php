4756329682e7a8e6cfae034e7c42858f044fa838
Andrew Nicholson <agdnicholson@gmail.com>
2022-02-12 13:27:29 +0000

    Initial commit.

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en-GB">
+    <head>
+        <meta charset="utf-8">
+        <title>My Blog</title>
+        <link rel="stylesheet" href="/app.css" />
+    </head>
+    <body>
+        <article>
+            <h1><a href="/post">My First Post</a></h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris efficitur in justo ac ultricies. Integer ornare accumsan mi at bibendum. Ut suscipit, massa ac varius gravida, dui nisl varius est, nec ultricies sem est commodo sapien. Mauris lacinia lacinia metus in tincidunt. Maecenas placerat eu orci vel tincidunt. Aenean non sapien ut purus venenatis egestas. Nam vel leo velit. Aliquam maximus, lorem vitae iaculis ultricies, turpis nibh finibus quam, tristique laoreet tellus ipsum in ligula. Praesent lectus nibh, elementum vitae consequat vitae, congue ut justo. Aliquam erat volutpat. Maecenas ac tempor diam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus diam odio, bibendum at feugiat et, fermentum vitae turpis. Integer consequat tortor metus, ut rutrum elit iaculis quis. Cras tempus dui sapien, ut vestibulum elit tristique bibendum. Ut sodales nisi nec eros dapibus tincidunt.
+            </p>
+        </article>
+        <article>
+            <h1><a href="/post">My Second Post</a></h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris efficitur in justo ac ultricies. Integer ornare accumsan mi at bibendum. Ut suscipit, massa ac varius gravida, dui nisl varius est, nec ultricies sem est commodo sapien. Mauris lacinia lacinia metus in tincidunt. Maecenas placerat eu orci vel tincidunt. Aenean non sapien ut purus venenatis egestas. Nam vel leo velit. Aliquam maximus, lorem vitae iaculis ultricies, turpis nibh finibus quam, tristique laoreet tellus ipsum in ligula. Praesent lectus nibh, elementum vitae consequat vitae, congue ut justo. Aliquam erat volutpat. Maecenas ac tempor diam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus diam odio, bibendum at feugiat et, fermentum vitae turpis. Integer consequat tortor metus, ut rutrum elit iaculis quis. Cras tempus dui sapien, ut vestibulum elit tristique bibendum. Ut sodales nisi nec eros dapibus tincidunt.
+            </p>
+        </article>
+        <article>
+            <h1><a href="/post">My Third Post</a></h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris efficitur in justo ac ultricies. Integer ornare accumsan mi at bibendum. Ut suscipit, massa ac varius gravida, dui nisl varius est, nec ultricies sem est commodo sapien. Mauris lacinia lacinia metus in tincidunt. Maecenas placerat eu orci vel tincidunt. Aenean non sapien ut purus venenatis egestas. Nam vel leo velit. Aliquam maximus, lorem vitae iaculis ultricies, turpis nibh finibus quam, tristique laoreet tellus ipsum in ligula. Praesent lectus nibh, elementum vitae consequat vitae, congue ut justo. Aliquam erat volutpat. Maecenas ac tempor diam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus diam odio, bibendum at feugiat et, fermentum vitae turpis. Integer consequat tortor metus, ut rutrum elit iaculis quis. Cras tempus dui sapien, ut vestibulum elit tristique bibendum. Ut sodales nisi nec eros dapibus tincidunt.
+            </p>
+        </article>
+    </body>
+</html>
